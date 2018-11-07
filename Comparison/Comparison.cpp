caced8fe602538bc8f2828586435f770ab1ca790
Mohammad-Mahdi1999 <44753085+Mohammad-Mahdi1999@users.noreply.github.com>
2018-11-07 19:31:23 +0330

    Start of Comparison

---
+++ Comparison/Comparison.cpp
@@ -0,0 +1,20 @@
+// In the name of God !
+//Comparison
+//V1.0
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	int num1, num2;
+	cout << "Enter two numbers :" << endl;
+	cin >> num1;
+	cin >> num2;
+	if (num1 < num2)
+		cout << "The bigger number is " << num2 << "!" << endl;
+	else 
+		cout << "The bigger number is " << num1 << "!" << endl;
+	system("pause");
+}
+//Created by /%++;; Mohammad-Mahdi Farah-bakhsh ;;++%\
